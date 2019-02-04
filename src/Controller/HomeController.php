9cc9262e188b7423c8166d55e4104cca7e2f44de
Thomas Lopez <thomaslopez@MacBook-Pro-de-Thomas.local>
2019-02-04 16:02:52 +0100

    Gestion des annonces, formulaires de création et d'édition

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,42 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller {
 
-    /**
-     * @Route("/hello/{prenom}/age/{age}", name="hello", requirements={"age"="\d+"})
-     * @Route("/hello/{prenom}", name="hello_prenom")
-     * @Route("/hello", name="hello_base")
-     * 
-     * Montre la page qui dit bonjour
-     *
-     * @return void
-     */
-    public function hello($prenom = "anonyme", $age = 0) {
-        return $this->render(
-            'hello.html.twig',
-            [
-                'prenom' => $prenom,
-                'age' => $age
-            ]
-        );
-    }
-
     /** 
      * @Route("/", name="homepage")
      */
     public function home() {
-        $prenoms = ["Thomas" => 26, "Camille" => 25, "Georges" => 55, "Léo" => 5];
 
-        return $this->render(
-            'home.html.twig',
-            [
-                'title' => "Bonjour à tous !",
-                'age' => 12,
-                'tableau' => $prenoms
-            ]
-        );
+        return $this->render('home.html.twig');
     }
      
-    
 }
 
 ?>
